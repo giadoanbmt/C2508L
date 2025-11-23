98e696f1aa76a5e63dd6c99262f940f8be6583ab
Quốc Gia <giadoanbmt@gmail.com>
2025-11-23 20:00:35 +0700

    Add files via upload

---
+++ Buoi1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World\n");
+	
+	int a = 0, b = 0;
+	
+	printf("Nhap vao so a: ");
+	scanf("%d", &a);
+	
+	printf("Nhap vao so b: ");
+	scanf("%d", &b);
+	
+	printf("Tong so a va b la: %d", a+b);
+	
+	return 0;
+}
